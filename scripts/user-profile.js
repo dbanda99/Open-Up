9a48e02c47f3721f40c9996dbcc234597bbf6e7a
David Banda <ldavidbandal@gmail.com>
2024-11-24 00:01:30 +0100

    Update user-profile.js

--- scripts/user-profile.js
+++ scripts/user-profile.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const githubMembersApiUrl = "https://api.github.com/repos/dbanda99/open-up-data/contents/members.json";
     const githubComplimentsApiUrl = "https://api.github.com/repos/dbanda99/open-up-data/contents/compliment-posts.json";
-    const githubToken = "Bearer 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";
+    const githubToken = "Bearer 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";
 
     // Toggle the visibility of the Edit Profile Window
     const toggleEditProfile = () => {
